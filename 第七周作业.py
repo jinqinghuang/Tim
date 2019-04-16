097a137d74db15f31d94e7b956c2012d9942d603
jinqinghuang <49049055+jinqinghuang@users.noreply.github.com>
2019-04-17 02:35:15 +0800

    Add files via upload

---
+++ 第七周作业.py
@@ -0,0 +1,23 @@
+'''
+name: 黄锦清
+student number: 1619100043
+function：读取有用数据并作折线图
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import matplotlib
+font = {'family':'SimHei'}
+matplotlib.rc('font',**font)
+data = pd.read_csv('35.csv')
+row = data.columns[5:20]
+plt.xlabel('date')
+plt.ylabel('score')
+for i in range(len(data['姓名'])):
+    list1 = data.iloc[i,5:20]
+    plt.ylim(0,100)
+    plt.plot(row,list1,'-',marker='o')
+    plt.title(data.iloc[i,0])
+    plt.xticks(rotation=45)
+    plt.show()
